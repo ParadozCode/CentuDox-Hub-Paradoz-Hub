b062ca444b55e39ac3749702bb3cf4a8f73c0dfe
ParadozCode <simpelscriptblox@gmail.com>
2025-08-29 06:35:09 +0530

    Update Paradoz Hub.lua

--- Paradoz Hub.lua
+++ Paradoz Hub.lua
@@ -1,3 +1,16 @@
 
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ParadozCode/ParadozHub-/refs/heads/main/CentuDox.lua"))()
+
+local discordLink = "https://discord.gg/arczFu2eXt"
+
+if setclipboard then
+    setclipboard(discordLink)
+elseif toclipboard then
+    toclipboard(discordLink)
+end
+
+
+LocalPlayer:Kick("CentuDox Hub\nScript is now updated to newer updates and will no longer receive updates.\n\nWatch Sherman's new videos and join the Discord:\n" .. discordLink)
+
