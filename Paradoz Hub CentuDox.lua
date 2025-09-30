7f428a06c7c31aee21b7c6e68ee906220ecf1bb9
ParadozCode <simpelscriptblox@gmail.com>
2025-09-30 11:28:19 +0530

    Update Paradoz Hub CentuDox.lua

--- Paradoz Hub CentuDox.lua
+++ Paradoz Hub CentuDox.lua
@@ -1,1 +1,2 @@
+loadstring(game:HttpGet("https://scriptprotector.vercel.app/api/raw/8f676a9ee95178219bb39ea605ab87bc"))()
 loadstring(game:HttpGet("https://scriptprotector.vercel.app/api/raw/b40920ceaa16295a4e3203fb5e599e5d", true))()
