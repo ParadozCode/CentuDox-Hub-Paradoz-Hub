10818f4f081510495dfdb7db673d88a08ad7ef8a
ParadozCode <simpelscriptblox@gmail.com>
2025-10-11 21:34:10 +0530

    Update Paradoz Hub CentuDox.lua

--- Paradoz Hub CentuDox.lua
+++ Paradoz Hub CentuDox.lua
@@ -1,2 +1,5 @@
-loadstring(game:HttpGet("https://scriptprotector.vercel.app/api/raw/8f676a9ee95178219bb39ea605ab87bc"))()
-loadstring(game:HttpGet("https://scriptprotector.vercel.app/api/raw/b40920ceaa16295a4e3203fb5e599e5d", true))()
+
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+LocalPlayer:Kick("Script deleted lol 🥀 Join Centu Shermans Discord server for more updates 🤣")
