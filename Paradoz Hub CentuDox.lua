40794bda6680fc84a2eac45aff50d74796773d3b
ParadozCode <simpelscriptblox@gmail.com>
2025-10-12 13:43:26 +0530

    Update Paradoz Hub CentuDox.lua

--- Paradoz Hub CentuDox.lua
+++ Paradoz Hub CentuDox.lua
@@ -1,2 +1,4 @@
-
+--main
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ParadozCode/CentuDox-Hub-Paradoz-Hub/refs/heads/main/CentuDox%20V2.lua", true))()
+-- jump hack
+loadstring(game:HttpGet("https://scriptprotector.vercel.app/api/raw/f6c1790c6b6899f0b1f2f3611b7684e0", true))()
